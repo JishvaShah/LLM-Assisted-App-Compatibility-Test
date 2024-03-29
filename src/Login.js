0bd74c52dde6cf94074cc4bd852c862333699e9d
Jishva Shah <shah.jish@Northeastern.edu>
2024-03-28 19:22:08 -0700

    Adding Login and Register UI.

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import { Link } from 'react-router-dom'; 
+import './Login.css';
 
 function Login() {
+
+  const [userID, setUserID] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log('Submitted:', { userID, password });
+  };
+
   return (
     <div className='main'>
       <Header />
-      <h1>Login</h1>
+      <div className='content'>
+          <p className='contentText'>Login</p>
+          <div className='loginForm'>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="userID">UserID:</label>
+              <input
+                type="text"
+                id="userID"
+                name="userID"
+                value={userID}
+                onChange={(event) => setUserID(event.target.value)}
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <input type="submit" value="Login" />
+            </form>
+          </div>
+          <p className='contentText'>New user? <Link className="registerLink" to="/register">Register here</Link></p>
+      </div>
       <Footer />
     </div>
   );
--- src/Register.js
+++ src/Register.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import { Link } from 'react-router-dom'; 
+import './Register.css';
 
 function Register() {
+
+  const [userID, setUserID] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log('Submitted:', { userID, password });
+  };
+
   return (
     <div className='main'>
       <Header />
-      <h1>Register</h1>
+      <div className='content'>
+          <p className='contentText'>Register</p>
+          <div className='regForm'>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="userID">UserID:</label>
+              <input
+                type="text"
+                id="userID"
+                name="userID"
+                value={userID}
+                onChange={(event) => setUserID(event.target.value)}
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <input type="submit" value="Register" />
+            </form>
+          </div>
+          <p className='contentText'>Existing user? <Link className="loginLink" to="/login">Login here</Link></p>
+      </div>
       <Footer />
     </div>
   );
