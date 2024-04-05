bc418ba16fb35796135c6aff8ed6ca9649819b46
Jishva Shah <shah.jish@Northeastern.edu>
2024-04-04 17:27:21 -0700

    Password verification and UI changes made.

--- src/Login.js
+++ src/Login.js
@@ -1,47 +1,49 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
-import { Link } from 'react-router-dom'; 
 import './Login.css';
 
 function Login() {
-  const [userID, setUserID] = useState('');
+  const [employeeID, setEmployeeID] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      console.log("emp:"+employeeID+" pwd:"+password);
       const response = await fetch('http://localhost:8000/login/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          employee_id: userID,
+          employee_id: employeeID,
           password: password
         })
       });
-      
+
       if (response.ok) {
         const data = await response.json();
-        //check
-        console.log('Login Successful');
         localStorage.setItem('accessToken', data.access);
         localStorage.setItem('refreshToken', data.refresh);
         navigate('/');
+        toast.success('Login Successful');
       } else {
-        //check
-        console.error('Login Failed');
+        const errorData = await response.json();
+        toast.error(errorData.error || 'Login Failed');
       }
 
-      setUserID('');
+      setEmployeeID('');
       setPassword('');
 
     } catch (error) {
-      //check
       console.error('Error:', error);
+      toast.error('An error occurred. Please try again later.');
     }
   };
 
@@ -52,13 +54,16 @@ function Login() {
           <p className='contentText'>Login</p>
           <div className='loginForm'>
             <form onSubmit={handleSubmit}>
-              <label htmlFor="userID">UserID:</label>
+              <label htmlFor="employeeID">Employee ID:</label>
               <input
                 type="text"
-                id="userID"
-                name="userID"
-                value={userID}
-                onChange={(event) => setUserID(event.target.value)}
+                id="employeeID"
+                name="employeeID"
+                value={employeeID}
+                onChange={(event) => setEmployeeID(event.target.value)}
+                pattern="[a-zA-Z0-9]+"
+                title="Please enter only letters and numbers"
+                required
               />
               <label htmlFor="password">Password:</label>
               <input
@@ -67,6 +72,7 @@ function Login() {
                 name="password"
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
+                required
               />
               <input type="submit" value="Login" />
             </form>
@@ -74,6 +80,7 @@ function Login() {
           <p className='contentText'>New user? <Link className="registerLink" to="/register">Register here</Link></p>
       </div>
       <Footer />
+      <ToastContainer />
     </div>
   );
 }
